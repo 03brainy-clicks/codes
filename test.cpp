57240500871e0658d45da38f8a879d173f369605
Shivam <chadrakant.kushwah2019@sait.ac.in>
2021-01-05 09:43:48 +0530

    data entered

---
+++ test.cpp
@@ -0,0 +1,20 @@
+/*
+55.Write a program to find LCM of two numbers.
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	
+	int n,s=0;
+	cout<<"enter no. digit : ";
+	cin>>n;
+	
+	for(int i = 1 ; i < n; i++){
+		for(int j < )
+	}
+	cout<<endl;
+	cout<<s;
+	return 0;
+}
